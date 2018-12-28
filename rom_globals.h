ed7f30f99472b311c770a5008bcf07ebedde9666
Berstenmacher <pi@raspberrypi>
2018-12-28 21:18:30 +0100

    jkhff

--- rom_globals.h
+++ rom_globals.h
@@ -34,8 +34,9 @@ constexpr auto _s_p_us =	ratio_value<std::micro>();	//seconds per micro second
 constexpr auto _s_p_ns =	ratio_value<std::nano>();	//seconds per nano second
 constexpr auto _s_p_ms =	ratio_value<std::milli>();	//seconds per milli second
 
-constexpr auto _one {_milli / _milli};//divide a number by itself to get 1.0; 	floating point 1.0
-constexpr auto _zero{_milli - _milli};//subtract a number from itslf to get 0.0;floating point 0.0
+constexpr auto _one {_milli / _milli}; //divide a number by itself to get 1.0; 	floating point 1.0
+constexpr auto _zero{_milli - _milli}; //subtract a number from itslf to get 0.0;floating point 0.0
+constexpr auto _neg_one {_zero - _one}; //you get it; 	floating point -1.0
 
 #ifndef TRUE	//alternative to keyword "true"
 constexpr uint8_t TRUE (_zero < _one); 	//yes that's true ;-)
@@ -54,11 +55,22 @@ constexpr std::complex<decltype(_one)> _j(_zero,_one);  //sqare root of (-1)
 constexpr uint8_t _HIGH{uint8_t(_one)};		//integer 1
 constexpr uint8_t _LOW {uint8_t(_zero)};	//integer 0
 
-constexpr double _EARTH_G(9.80665);	//[(m/(s*s))/G]  Surface acceleration on Earth (average Value)
-constexpr double _GAUSS_P_TESLA(10.0 * _kilo);		//unit conversion for magnetic flux density
-constexpr double _TESLA_P_GAUSS(_one/_GAUSS_P_TESLA);	//unit conversion for magnetic flux density
+constexpr auto _EARTH_G(9.80665);	//[(m/(s*s))/G]  Surface acceleration on Earth (average Value)
+constexpr auto _GAUSS_P_TESLA(10.0 * _kilo);		//unit conversion for magnetic flux density
+constexpr auto _TESLA_P_GAUSS(_one/_GAUSS_P_TESLA);	//unit conversion for magnetic flux density
+constexpr auto _EARTH_RADIUS(6371000.785);             //meter  (average Value)
 
-constexpr auto _M_P_FEET(double(0.3048));		//speed unit conversion factor
+constexpr auto _ROM_PI_FUNC(void)->decltype(_neg_one){	//copute pi at compiletime (3.1415..............)
+//acos(_neg_one);					//acos of -1.0 should be pi or -pi
+return (acos(_neg_one)>_zero)?(acos(_neg_one)):(_neg_one*acos(_neg_one));	//return the positive value
+}
+
+constexpr auto _ROM_PI{_ROM_PI_FUNC()};	
+
+constexpr auto _GRAD_P_RAD (180.0/_ROM_PI);	//conversion factor of angle units
+constexpr auto _RAD_P_GRAD (_ROM_PI/180.0);	//conversion factor of angle units
+
+constexpr auto _M_P_FEET(double(0.3048));	//speed unit conversion factor
 constexpr auto _KNOT_P_MPS(double(1.943844));  	//speed unit conversion factor
 constexpr auto _MPS_P_KNOT (_one/_KNOT_P_MPS);	//speed unit conversion factor
 
@@ -78,8 +90,6 @@ constexpr auto _uint32_t_max 	{std::numeric_limits<uint32_t>::max()};
 constexpr auto _uint64_t_max 	{std::numeric_limits<uint64_t>::max()};
 
 constexpr auto _streamsize_max	{std::numeric_limits <std::streamsize>::max()};
-
-
 
 
 }	//namespace rom
--- rom_wiringpin.h
+++ rom_wiringpin.h
@@ -5,8 +5,9 @@
 #include "rom_error.h"
 #include "wiringPi.h"
 
-uint64_t _WIRINGPI_IS_SETUP{0};
+namespace rom{
 
+uint64_t _WIRINGPI_IS_SETUP{0};	//ToDo make it a static membervariable
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -17,36 +18,30 @@ uint8_t val;    //last output value: 0->low  1->high  3->input
 rom::sheepdelay delay;  //function object for timing
 
 public:
-friend std::ostream& operator<<(std::ostream& os, const wiringpin& pin) {return os << "WirinPi: "<< pin.wp;}
+friend std::ostream& operator<<(std::ostream& os, const wiringpin& pin) {return os << "WirinPi: "<<uint16_t(pin.wp);}
 
-wiringpin(int8_t nr):wp(nr),delay{} {
-val = 3;
-if (_WIRINGPI_IS_SETUP++ == 0)   {wiringPiSetup();}
-if ((wp >=0)  && (wp<=29))      {pinMode(wp,INPUT);}
-else {rom_error("Es wurde versucht einen GPIO anzusprechen, welcher nicht existiert");}
+wiringpin(int8_t nr):wp(nr),delay{},val{3} {
+if (_WIRINGPI_IS_SETUP++ == 0)	{wiringPiSetup();}
+if ((wp>=0)  && (wp<=29))	{pinMode(wp,INPUT);}
+else {rom::error("Pin number "+std::to_string(wp)+" is not defined in wirinPi-Library.\n");}
 }
 
 ~wiringpin(void) {pinMode(wp,INPUT);}//high impendance input state for abandoned pins is the safest choice
 
-inline void pullhi() {
-if (val == 3)           {pinMode(wp,OUTPUT);}
-if (val != rom::_HIGH)        {digitalWrite(wp,rom::_HIGH);}
-val =1;
-}
+inline void pullhi() {if (val != rom::_HIGH)	{digitalWrite(wp,rom::_HIGH);}}
 
-inline void pulllo() {
-if (val == 3)           {pinMode(wp,OUTPUT);}
-if (val != rom::_LOW)         {digitalWrite(wp,rom::_LOW);}
-val =0;
-}
+inline void pulllo() {if (val != rom::_LOW)	{digitalWrite(wp,rom::_LOW);}}
+
 inline void write(uint8_t bit) {
-if (bit ==0)            {pulllo();}
-else if (bit ==1)       {pullhi();}
-else                    {rom_error("Es wurde versucht ein bit an einen GPIO zu das nicht 0 od.1 ist!!!");}
+{if 	(val == 3)	{pinMode(wp,OUTPUT);}}
+{if 	(bit ==_LOW)	{pulllo();}
+else if (bit ==_HIGH)	{pullhi();}
+else			{rom::error("");}}
+val =bit;
 }
 
 inline void flow() {
-if (val != 3)     {pinMode(wp,INPUT);}
+if (val != 3)	{pinMode(wp,INPUT);}
 val =3;
 }
 
@@ -57,23 +52,19 @@ return digitalRead(wp);
 
 inline uint8_t flow_and_wait(double dt_sec=0.1) {//release gpio and wait for i2c pullup to pull line HIGH
 flow();                                         //this may take some time if a i2c slave does clock-stretching on
-double beg = rom_mashinetime() + dt_sec;        //a scl line //eine sekunde in der zukunft
+double beg = rom::mashinetime() + dt_sec;        //a scl line //eine sekunde in der zukunft
 do      {
-        if (rom_mashinetime()>beg) {
+        if (rom::mashinetime()>beg) {
                 std::cout<<"Der GPIO mit Nummer "<<uint16_t(wp)<<" wiringpi wird seit "<<dt_sec<<" Sekunden von einem";
                 std::cout<<" anderen Geraet auf Low gehalten oder von keinem Pullup Resistor auf HIGH gezogen\n";
                 return 1; //error?  = true
                 }
-        } while (read() != ROM_HIGH);
+        } while (read() != rom::_HIGH);
 return 0;  //Error? == false
 }
 };	//wiringpin
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
-
+}	//namespace rom
 #endif

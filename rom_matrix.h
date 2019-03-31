afdbce5b9a045b1ebc61ff5eff520deea720e740
Buerstenmacher <roman_lechner@yahoo.com>
2019-03-31 23:33:33 +0200

    new function for n-dimensional crossproduct

--- rom_matrix.h
+++ rom_matrix.h
@@ -5,34 +5,56 @@
 
 namespace rom {
 
+//Helper Functions:
 int8_t parity_of_permutation(size_t inp) {return (inp%2)?-1:1;}
 
+//test if an 2d vector is rectangular
+template<class blt> //should work for every built in type
+uint8_t is_rectangular(const std::vector<std::vector<blt>>& in) {
+for (auto& elem:in) {if (elem.size()!=in.size()) {return 0;}}
+return 1;
+}
+
+template<class blt> //should work for every built in type
+//this should create a vector of columns if input is a vector of rows and vise-versa
+std::vector<std::vector<blt>> row_column_mirror(const std::vector<std::vector<blt>>& in) {
+if (is_rectangular(in)==0) {rom::error("expected an rectangular 2d vector");}
+auto ret{in}; //copy of input
+for (size_t a{0};a!=in.size();++a) {
+	for (size_t b{0};b!=in.at(a).size();++b) {
+		ret.at(b).at(a)=in.at(a).at(b); //mirror every value
+		}
+	}
+return ret;
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-//class Vector should represent an two dimensional matrix
+//class Matrix should represent an two dimensional matrix
 template<class flt=double>
 class Matrix{
+
 private:
 std::vector<flt> m_d;  //the two dimensions should be flatened to an one dimensional std::vector
 size_t m_rows;
 size_t m_columns;
 
+public:
 Matrix delete_row_and_column(size_t r,size_t c) const{
 std::vector<std::vector<flt>> ret;
 ret.resize(0);  //empty vector
 for (size_t row{0}; row!=rows();row++) {
         if (row!=r) {
-                ret.push_back({});
+                ret.push_back({});//insert a new row
                 for (size_t col{0}; col!=columns();col++) {
                         if(col!=c) {
-                                ret.back().push_back(at(row,col));
+                                ret.back().push_back(at(row,col));//add elements to new row
                                 }
                         }
                 }
         }
-return Matrix(ret);
+return Matrix(ret);//convert 2d vector to Matrix-Object
 }
 
-public:
 flt& at(size_t row_in,size_t column_in) {	//member access
 return m_d.at(row_in*m_columns + column_in);	//in informatics indices begin at 0!!
 }
@@ -48,7 +70,7 @@ size_t size(void) 	const {return m_columns * m_rows;}
 Matrix(void):m_d{0.0},m_rows{1},m_columns{1}  {}  	//default size is one, value is 0.0
 
 Matrix(const std::vector<std::vector<flt>>& inp):Matrix{} {//construction from 2 dimensional vector
-m_rows = inp.size();
+m_rows = inp.size();					//input has to be a vector of rows
 m_columns = inp.at(0).size();//ToDo: check if every row has the same number of columns
 m_d.resize(size());
 for (size_t r{0};r!=rows();r++) { 		//every row
@@ -60,9 +82,10 @@ for (size_t r{0};r!=rows();r++) { 		//every row
 
 flt det(void) const {//calculate the determinant of matrix with Laplace's formula
 if (rows()!=columns()) {error("Sorry cannot calculate determinant of a rectangular Matrix");}
-if (rows() < 2){error("Determinant is not defined if size of matrix is smaller than 2x2");}
+if (rows() < 1){error("Determinant is not defined if size of matrix is smaller than 1x1");}
 flt ret{0.0};
-if (rows()==2) {ret = at(0,0)*at(1,1)-at(0,1)*at(1,0);}//perform simple Sarrus(2x2) rule
+if (rows()==1) {ret = at(0,0);}//trivial case
+else if (rows()==2) {ret = at(0,0)*at(1,1)-at(0,1)*at(1,0);}//perform simple Sarrus(2x2) rule
 else 	{
 	for (size_t r{0};r!=rows();r++) {
 		flt mul{delete_row_and_column(r,0).det()};//recursion is slow but correct
@@ -74,11 +97,10 @@ else 	{
 return ret;
 }
 
-
 operator std::string() const {//make it easy to output internal data of vector class
 std::ostringstream o;
 for (size_t r{0};r<rows();++r) {
-	o << std::endl << "{ ";
+	o << std::endl << "{";
 	for (size_t c{0};c<columns();c++) {o << " (" << m_d.at(c+r*columns())<<")";}
 	o  << " }";
 	}
@@ -95,7 +117,7 @@ void rom_matrix_t(void){
 
 std::cout<<std::endl;
 std::cout <<"//////////////////////////////////////////////////////////////////////////////////"<<std::endl;
-std::cout <<"Testing the vector library: "<<std::endl;
+std::cout <<"Testing the matrix library: "<<std::endl;
 std::cout <<"//////////////////////////////////////////////////////////////////////////////////"<<std::endl;
 
 
@@ -110,13 +132,6 @@ rom::Matrix<float>  a{{	{5.0,9.0,3.0,7.0,1.0,5.0, 7 , 1 },
 			{9.0,2.0,6.0,2.0,5.0,9.0, 7 , 6 }	}};
 std::cout <<"Matrix"<< std::string(a) << std::endl;
 std::cout <<"Determinant "<< a.det() << std::endl;
-
-std::cout <<"Matrix"<< std::string(a=a.delete_row_and_column(1,1)) << std::endl;
-std::cout <<"Determinant "<< a.det() << std::endl;
-
-std::cout <<"Matrix"<< std::string(a=a.delete_row_and_column(1,1)) << std::endl;
-std::cout <<"Determinant "<< a.det() << std::endl;
-
 rom::Matrix<float>  b{{{4.0,3.0},{7.0,5.0}}};
 std::cout <<"Matrix"<< std::string(b) << std::endl << "Determinant: " << b.det();
 
--- rom_spacetime.h
+++ rom_spacetime.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <array>
 #include "rom_error.h"
+#include "rom_matrix.h"
 
 namespace rom {
 
@@ -16,6 +17,10 @@ std::array<flt,dim> m_xyz;
 
 public:
 Vector(const std::array<flt,dim>& in):m_xyz{in} {}//construct from std::array<flt>
+
+Vector(const Vector& in):m_xyz{in.m_xyz} {}	//copy
+
+Vector(void):m_xyz{} {for (auto& d:m_xyz) {d = flt(0.0);}}	//defaut construct to 0.0 length
 
 flt abs() const {		//get the magnitude of vector
 flt ret{0.0};
@@ -39,14 +44,10 @@ if (dim_in >= m_xyz.size()) {
 return m_xyz.at(dim_in);
 }
 
-Vector(const Vector& in):m_xyz{in.m_xyz} {}	//copy
-
 Vector& operator=(const Vector& in) {	//copy construct
 m_xyz = in.m_xyz;
 return *this;
 }
-
-Vector(void):m_xyz{} {for (auto& d:m_xyz) {d = flt(0.0);}}	//defaut construct to 0.0 length
 
 Vector operator*(flt in)const {	//multiply by scalar
 Vector ret{*this};
@@ -85,16 +86,35 @@ if (0.0 == absi)        {rom::error("Error at creating an unitvector.");}
 return ((*this)/absi);
 }
 
-static Vector e_x(uint16_t d_in) { 	//return the unit vector of dimension 0 to (n-1)
+static Vector e_x(uint16_t d_in) { 	//return the caconical unit vector of dimension 0 to (n-1)
 Vector ret{};				//default constructor should return us an all zero vector;
 ret.at(d_in) = flt(1.0);
 return ret;
 }
 
-//---------ToDo find a Formula for n dimensional spaces----------------
+//crossproduct for n-dimensional Vectors
 //https://de.wikipedia.org/wiki/Kreuzprodukt
-//this should calculate the crossproduct of n=3 dimensional vectors
 //the cross product needs (n-1) vectors (where n is the dimensionality) as input,
+static Vector crossp(const std::array<typename rom::Vector<flt,dim>,dim-1>& arr_in) {
+static_assert(dim >= 2,"Sorry, crossprodukt is not defined if the dimension is smaller than 2.");
+std::vector<std::vector<flt>> inv {};	//first copy everything into vector of vector of flt
+std::vector<flt> frstcol(dim,1.0);	//and create a vector of all 1's for first column
+inv.push_back(frstcol); 		//push the first column in our 2d vector
+for (auto& col:arr_in) {inv.push_back(std::vector<flt>(col));}//now everything is in vector of columns
+inv=row_column_mirror(inv);		//it's now a vector of rows
+auto matrix_in{Matrix<flt>(inv)};	//create a matrix from input
+rom::Vector<flt,dim> ret{};		//prepare a zero return rom::Vector
+for (size_t row{0};row!=dim;row++) {	//column is 0 row is running
+	flt mul {1.0};			//initialize to 1.0
+	mul = matrix_in.delete_row_and_column(row,0).det();	//multiply by determinant
+	mul *= matrix_in.at(row,0);	//multiply by 1.0 (obsolete)
+	mul *= parity_of_permutation(row+0);	//get the sign -1.0 or 1.0
+	ret.at(row) = mul;		//insert in return variable
+	}
+return ret;
+}
+
+/*   This would work for 3 dimensional Vectors and is obsolete
 static Vector crossp(const std::array<typename rom::Vector<flt,dim>,dim-1>& arr_in) {
 static_assert(dim == 3,"Sorry, we are unable to calculate the crossprodukt if the dimension is not 3.");
 auto a = arr_in.at(1);	//temporary variables save us some typing
@@ -102,9 +122,11 @@ auto in =arr_in.at(0);
 return Vector(	std::array<flt,3>{	(in.at(1)*a.at(2) - in.at(2)*a.at(1)),
 					(in.at(2)*a.at(0) - in.at(0)*a.at(2)),
 					(in.at(0)*a.at(1) - in.at(1)*a.at(0))	}	);
-}
+*/
+
 
 static Vector crossp(const rom::Vector<flt,dim>& a,const rom::Vector<flt,dim>& b) {//overlord
+static_assert(dim == 3,"Sorry, this simplification is only available for 3-dimensional spaces");
 return crossp(std::array<flt,2>{a,b});
 }
 
@@ -117,6 +139,12 @@ return res;
 static flt alpha(const Vector& a,const Vector& b) {//calculate the angle between two vectors 
 auto cosalpha = scalarp(a.unitvector(),b.unitvector());
 return acos(cosalpha);
+}
+
+operator std::vector<flt>() const {
+std::vector<flt> ret{};
+for (auto& e:m_xyz) {ret.push_back(e);}
+return ret;
 }
 
 operator std::string() const {//make it easy to output internal data of vector class
@@ -143,23 +171,44 @@ std::cout <<"///////////////////////////////////////////////////////////////////
 std::cout <<"Testing the vector library: "<<std::endl;
 std::cout <<"//////////////////////////////////////////////////////////////////////////////////"<<std::endl;
 
-rom::Vector<double, 3> vec1{std::array<double,3>{1.0,2.0,3.0}};
-rom::Vector<double, 3> vec2{std::array<double,3>{0.0,7.5,0.0}};
+rom::Vector<double, 3> vec1{{1.0,2.0,3.0}};
+rom::Vector<double, 3> vec2{{0.0,7.5,0.0}};
 rom::Vector<double, 3> vec3= vec1+vec2;
 std::cout << std::string(vec1) << "  +  "<<std::string(vec2 ) <<" = "<<std::endl;
 std::cout << std::string(vec3)<<std::endl;
+std::cout <<std::endl;
 
-rom::Vector<double, 3> vec5{std::array<double,3>{1.0,0.0,0.0}};
-rom::Vector<double, 3> vec4{std::array<double,3>{0.0,1.0,0.0}};
+rom::Vector<double, 3> vec4{{2.0,1.0,0.0}};
+rom::Vector<double, 3> vec5{{1.0,3.0,0.0}};
 auto result = rom::Vector<double,3>::crossp(std::array<rom::Vector<double,3>,2>{vec4,vec5});
-
 std::cout <<"Crossproduct of: "<< std::string(vec4)<< "   " << std::endl;
 std::cout <<"and              "<< std::string(vec5)<< " = " << std::endl;
 std::cout << std::string(result)<<std::endl;
-
 std::cout << rom::Vector<double,3>::alpha(vec5,vec4) << " rad" << std::endl;
 std::cout << rom::Vector<double,3>::alpha(vec4,result) << " rad" << std::endl;
 std::cout << rom::Vector<double,3>::alpha(result,vec5) << " rad" << std::endl;
+std::cout <<std::endl;
+
+rom::Vector<double, 2> vec6{{2.0,-1.0}};
+auto result2 = rom::Vector<double,2>::crossp(std::array<rom::Vector<double,2>,1>{vec6});
+std::cout <<"Crossproduct of: "<< std::string(vec6)<< " = " << std::endl;
+std::cout << std::string(result2)<<std::endl;
+std::cout << rom::Vector<double,2>::alpha(vec6,result2) << " rad" << std::endl;
+std::cout <<std::endl;
+
+rom::Vector<float, 4> vec7{{2.0,0.0,0.0,0.0}};
+rom::Vector<float, 4> vec8{{0.0,3.0,0.0,0.0}};
+rom::Vector<float, 4> vec9{{0.0,0.0,4.0,0.0}};
+auto result3 = rom::Vector<float,4>::crossp({vec7,vec8,vec9});
+std::cout <<"Crossproduct of: "<< std::string(vec7)<< "   " << std::endl;
+std::cout <<"Crossproduct of: "<< std::string(vec8)<< "   " << std::endl;
+std::cout <<"and              "<< std::string(vec9)<< " = " << std::endl;
+std::cout << std::string(result3)<<std::endl;
+std::cout << rom::Vector<float,4>::alpha(vec7,result3) << " rad" << std::endl;
+std::cout << rom::Vector<float,4>::alpha(result3,vec8) << " rad" << std::endl;
+std::cout << rom::Vector<float,4>::alpha(result3,vec9) << " rad" << std::endl;
+std::cout <<std::endl;
+
 
 
 }
